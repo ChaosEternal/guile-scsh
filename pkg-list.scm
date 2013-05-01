e8ac39f649c253812dc723ee9c4b24fc9da5a747
Chaos Eternal <chaos@shlug.org>
2013-05-01 20:38:44 +0800

    Added package definition file

---
+++ pkg-list.scm
@@ -0,0 +1,13 @@
+;; Copyright (C) 2013 Chaos Eternal <chaoseternal@gmail.com>
+;; See file COPYING. 
+
+(package (guile-scsh (0 1))
+	 (provides scsh)
+	 (depends (srfi-8) (srfi-9) (srfi-13) (srfi-14))
+	 (synopsis "scheme shell ported to guile-2.x")
+	 (description
+	  "A port of scsh (scheme shell) to guile."
+	  )
+	 (homepage "http://gitorious.org/guile-scsh")
+	 (libraries "scsh")
+  (documentation "docs" "README" "COPYING" "USAGE"))
