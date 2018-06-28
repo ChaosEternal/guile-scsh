dff27b4e5ff2bebbb11e2691e27535f8075cf1ac
Chaos Eternal <chaoseternal@gmail.com>
2018-06-28 17:07:26 +0800

    fix for guile2.0.9

--- scsh/weak.scm
+++ scsh/weak.scm
@@ -5,6 +5,6 @@
   :export (make-weak-pointer weak-pointer-ref weak-pointer?))
 
 (define (make-weak-pointer x) (make-weak-vector 1 x))
-(define (weak-pointer-ref  x) (weak-vector-ref x 0))
+(define (weak-pointer-ref  x) (vector-ref x 0))
 (define (weak-pointer? x)
   (weak-vector? x))
